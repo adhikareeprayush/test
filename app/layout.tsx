36e25a53404c3c91e3fd5542a2a5246b73a1b233
adhikareeprayush <adhikareeprayush@gmail.com>
2025-11-16 15:43:04 +0545

    design fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "@/components/reusable/Header";
 import Nav from "@/components/reusable/Nav";
 import Footer from "@/components/reusable/Footer";
 
